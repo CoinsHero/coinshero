3c4d267216119b3cb48ca5287d691e72d719eb79
Or Kazaz <or.kazaz@autodesk.com>
2017-06-12 22:06:26 +0300

    Disabled SVGs

--- webpack-config/webpack.shared.config.js
+++ webpack-config/webpack.shared.config.js
@@ -7,7 +7,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanPlugin = require('clean-webpack-plugin');
 const InlineChunkWebpackPlugin = require('html-webpack-inline-chunk-plugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
-const InterpolateLoaderOptionsPlugin = require('interpolate-loader-options-webpack-plugin');
+// const InterpolateLoaderOptionsPlugin = require('interpolate-loader-options-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const webpackUtils = require('./webpack.utils');
@@ -99,18 +99,18 @@ const config = {
         test: /\.(eot|ttf|woff|woff2)$/,
         loader: 'file-loader'
       },
-      {
-        test: /\.svg$/,
-        use: [
-          {
-            loader: '@adsk/bim360-svg-store-transform-loader'
-          },
-          {
-            loader: 'svgo-loader',
-            options: require('./svgo.plugins')
-          }
-        ]
-      },
+      // {
+      //   test: /\.svg$/,
+      //   use: [
+      //     {
+      //       loader: '@adsk/bim360-svg-store-transform-loader'
+      //     },
+      //     {
+      //       loader: 'svgo-loader',
+      //       options: require('./svgo.plugins')
+      //     }
+      //   ]
+      // },
       {
         test: /\.(mp4|webm)$/,
         loader: 'url-loader',
@@ -160,14 +160,14 @@ const config = {
     new InlineChunkWebpackPlugin({
       inlineChunks: ['manifest'],
       quiet: true
-    }),
-    new InterpolateLoaderOptionsPlugin({
-      loaders: [{
-        name: 'svgo-loader',
-        // 25 is the index of the cleanupIDs plugin in the plugins array.
-        include: ['plugins.25.cleanupIDs.prefix']
-      }]
     })
+    // new InterpolateLoaderOptionsPlugin({
+    //   loaders: [{
+    //     name: 'svgo-loader',
+    //     // 25 is the index of the cleanupIDs plugin in the plugins array.
+    //     include: ['plugins.25.cleanupIDs.prefix']
+    //   }]
+    // })
   ]
 };
 
