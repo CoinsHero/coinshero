93956ac3af9bee89a9290972a473a5871e573718
Or Kazaz <or.kazaz@autodesk.com>
2017-06-14 09:34:23 +0300

    Added support for RTL language and LTR + i18n. Will need to handle switching between them better in the future

--- src/chunks/main-chunk.js
+++ src/chunks/main-chunk.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {setLanguage, getLanguage, languages} from '../i18n';
 
-import {Main} from '../components/Main';
+import Main from '../components/Main';
 
 const runMain = () => {
+  // TODO: Take that from the URL / where ever is needed to handle it - https://github.com/kazazor/coinsmarket/issues/8
+  // TODO: We also need to decide on to show RTL or LTR - https://github.com/kazazor/coinsmarket/issues/7
+  // Setting the locale of the user
+  setLanguage(languages.he.code);
+
   ReactDOM.render(
-    <Main />,
+    <Main isRTL={getLanguage().isRTL}/>,
     document.getElementById('root')
   );
 };
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,27 @@
 import React, {Component} from 'react';
+import T from 'i18n-react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
 
 import '../styles/components/_Main.scss';
 
-export class Main extends Component {
+class Main extends Component {
   render() {
+    const cx = classnames(
+      'Main',
+      {'Main--rtl': this.props.isRTL}
+    );
+
     return (
-      <span>Hello man!!!!</span>
+      <div className={cx}>
+        <span>{T.translate('HELLO_WORLD')}</span>
+      </div>
     );
   }
 }
+
+Main.propTypes = {
+  isRTL: PropTypes.bool
+};
+
+export default Main;
