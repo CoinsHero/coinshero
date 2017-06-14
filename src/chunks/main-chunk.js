228f95aaa7f64cd88af93661ae83cb5d6e022767
Or Kazaz <or.kazaz89@gmail.com>
2017-06-14 23:07:48 +0300

    Kazaz/support i18n rtl ltr (#9)
    * Added support for RTL language and LTR + i18n. Will need to handle switching between them better in the future

--- src/chunks/main-chunk.js
+++ src/chunks/main-chunk.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {setLanguage, getLanguage, languages} from '../i18n';
 
-import {Main} from '../components/Main';
+import Main from '../components/Main';
 
 const runMain = () => {
+  // TODO: Take that from the URL / where ever is needed to handle it + LTR/RTL - https://github.com/kazazor/coinsmarket/issues/8
+  // Setting the locale of the user
+  setLanguage(languages.he.code);
+
   ReactDOM.render(
-    <Main />,
+    <Main isRTL={getLanguage().isRTL}/>,
     document.getElementById('root')
   );
 };
