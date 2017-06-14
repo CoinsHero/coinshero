b962c9dc2635511f35cd333fac2c417f892a5918
Or Kazaz <or.kazaz@autodesk.com>
2017-06-14 09:38:58 +0300

    Removed comment

--- src/chunks/main-chunk.js
+++ src/chunks/main-chunk.js
@@ -5,8 +5,7 @@ import {setLanguage, getLanguage, languages} from '../i18n';
 import Main from '../components/Main';
 
 const runMain = () => {
-  // TODO: Take that from the URL / where ever is needed to handle it - https://github.com/kazazor/coinsmarket/issues/8
-  // TODO: We also need to decide on to show RTL or LTR - https://github.com/kazazor/coinsmarket/issues/7
+  // TODO: Take that from the URL / where ever is needed to handle it + LTR/RTL - https://github.com/kazazor/coinsmarket/issues/8
   // Setting the locale of the user
   setLanguage(languages.he.code);
 
