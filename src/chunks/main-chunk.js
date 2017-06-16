66fd182840e65df81a020700f42de65ce5758f6c
Omri Aharon <omri.aharon@autodesk.com>
2017-06-16 16:27:53 +0300

    Merge

--- src/chunks/main-chunk.js
+++ src/chunks/main-chunk.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {setLanguage, getLanguage, languages} from '../i18n';
 import { Provider } from 'react-redux';
 
+import Main from '../components/Main';
 import store from '../redux/store';
 
-import Main from '../components/Main';
-
 const runMain = () => {
+  // TODO: Take that from the URL / where ever is needed to handle it + LTR/RTL - https://github.com/kazazor/coinsmarket/issues/8
+  // Setting the locale of the user
+  setLanguage(languages.he.code);
+
   ReactDOM.render(
     <Provider store={ store }>
-      <Main />
+      <Main isRTL={getLanguage().isRTL}/>
     </Provider>,
     document.getElementById('root')
   );
---
+++ src/i18n.js
@@ -0,0 +1,39 @@
+import T from 'i18n-react';
+
+// Why not use dynamic loading?
+// We cannot use dynamic loading in a dynamic require statement. Which means we cannot decide what file to load dynamically at
+// runtime. This will have to be something to improve on server side rendering if we'll choose to do so.
+import * as englishUSTexts from './locale/en.yml';
+import * as hebrewTexts from './locale/he.yml';
+
+let currentLang;
+export const DEFAULT_LANGUAGE = {
+  code: 'en'
+};
+
+export const languages = {
+  [DEFAULT_LANGUAGE.code]: DEFAULT_LANGUAGE,
+  'he': {
+    code: 'he',
+    isRTL: true
+  }
+};
+
+const languagesTexts = {
+  [languages.en.code]: englishUSTexts,
+  [languages.he.code]: hebrewTexts
+};
+
+/**
+ * @param {string} lang - Language code
+ */
+export const setLanguage = (lang) => {
+  if (!(lang in languages)) {
+    lang = DEFAULT_LANGUAGE.code;
+  }
+
+  currentLang = languages[lang];
+  T.setTexts(Object.assign({}, languagesTexts[DEFAULT_LANGUAGE.code], languagesTexts[currentLang.code]));
+};
+
+export const getLanguage = () => currentLang;
