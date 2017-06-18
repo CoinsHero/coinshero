0e7b1d450bada8f198448328b6ca96947a87157f
Or Kazaz <or.kazaz@autodesk.com>
2017-06-18 22:09:26 +0300

    Clean up some comments

--- src/helpers/getSiteTheme.js
+++ src/helpers/getSiteTheme.js
@@ -2,44 +2,6 @@ import { createMuiTheme } from 'material-ui/styles';
 import { grey, pink, red } from 'material-ui/styles/colors';
 import createPalette from 'material-ui/styles/palette';
 
-// // https://material-ui-1dab0.firebaseapp.com/customization/themes#the-other-variables
-// const muiTheme = getMuiTheme({
-//   borderRadius: 2,
-//   palette: {
-//     primary1Color: primaryColor,
-//     primary2Color: cyan700,
-//     primary3Color: grey600,
-//     accent1Color: pink700,
-//     accent2Color: pink500,
-//     accent3Color: pink400,
-//     textColor: fullWhite,
-//     secondaryTextColor: fade(fullWhite, 0.7),
-//     alternateTextColor: fade(fullWhite, 0.9),
-//     canvasColor: primaryColor,
-//     borderColor: fade(fullWhite, 0.3),
-//     disabledColor: fade(fullWhite, 0.3),
-//     pickerHeaderColor: fade(fullWhite, 0.12),
-//     clockCircleColor: fade(fullWhite, 0.12)
-//   }
-// });
-//
-// const overrides = {
-//   appBar: {
-//     color: muiTheme.palette.primary1Color,
-//     textColor: muiTheme.palette.alternateTextColor
-//   },
-//   tabs: {
-//     backgroundColor: muiTheme.palette.primary1Color,
-//     textColor: muiTheme.palette.secondaryTextColor,
-//     selectedTextColor: muiTheme.palette.alternateTextColor
-//   },
-//   refreshIndicator: {
-//     strokeColor: muiTheme.palette.primary1Color,
-//     loadingStrokeColor: muiTheme.palette.accent2Color
-//   }
-// };
-//
-
 const getSiteTheme = (isRTL) => {
   const theme = createMuiTheme({
     direction: isRTL ? 'rtl' : 'ltr',
