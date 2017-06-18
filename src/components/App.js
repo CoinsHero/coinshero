bf365879ce5e5b2e4e5cd519fce01ed8e95d16de
Or Kazaz <or.kazaz89@gmail.com>
2017-06-18 22:14:27 +0300

    Changes to the new material-ui (#53)
    * Initial changes towards the new material-ui
    
    * Spacing
    
    * Fixed spacing
    
    * Update fonts
    
    * Updated all to the new material-ui
    
    * Update table and background color
    
    * Clean up some comments

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,41 @@
 import React, {Component} from 'react';
-import classnames from 'classnames';
+import classnamesjss from '../helpers/classnamesjss';
 import PropTypes from 'prop-types';
+import { withStyles, createStyleSheet } from 'material-ui/styles';
 
 import NavigationHeader from './NavigationHeader';
 import CoinsTable from './CoinsTable';
 
-import '../styles/components/_App.scss';
+const styleSheet = createStyleSheet('App', (theme) => ({
+  'root': {
+    direction: 'ltr'
+  },
+  'root--rtl': {
+    direction: 'rtl'
+  },
+  'root__container': {
+    display: 'flex',
+    flexDirection: 'column',
+    marginTop: theme.spacing.unit * 2.5,
+    alignItems: 'center'
+  }
+}));
 
 class App extends Component {
   render() {
-    const cx = classnames(
-      'App',
-      {'App--rtl': this.props.locale.isRTL}
+    const classes = this.props.classes;
+    const cx = classnamesjss(classes,
+      'root',
+      {'root--rtl': this.props.locale.isRTL}
     );
 
-    // //////// MOCK DATA //////////
+    // ///////// MOCK DATA //////////
     // TODO: Delete as part of https://github.com/kazazor/coinsmarket/issues/15
     const mockPairs = [];
     const USD = require('../models/currencies/USD').default;
     const ValuePair = require('../models/ValuePair').default;
 
-    for (let index = 1; index < 100; index++) {
+    for (let index = 1; index < 1000; index++) {
       let pair = {
         rank: index,
         name: index % 2 === 0 ? `Test name - ${index}` : `שם בעברית קצת - ${index} עם!`,
@@ -35,12 +50,12 @@ class App extends Component {
       mockPairs.push(ValuePair.parse(pair, this.props.locale));
     }
 
-    // //////// MOCK DATA //////////
+    // ///////// MOCK DATA //////////
 
     return (
       <div className={cx}>
         <NavigationHeader locale={this.props.locale} />
-        <div className='App__container'>
+        <div className={classes['root__container']}>
           <CoinsTable valuePairs={mockPairs} />
         </div>
       </div>
@@ -49,10 +64,11 @@ class App extends Component {
 }
 
 App.propTypes = {
+  classes: PropTypes.object.isRequired,
   locale: PropTypes.shape({
     code: PropTypes.string,
     isRTL: PropTypes.bool
   })
 };
 
-export default App;
+export default withStyles(styleSheet)(App);
