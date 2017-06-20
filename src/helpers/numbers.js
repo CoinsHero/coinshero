b4b256241aeff048173db0fa33b12b0e28d91d92
Or Kazaz <or.kazaz@autodesk.com>
2017-06-20 21:38:59 +0300

    Typo

--- src/helpers/numbers.js
+++ src/helpers/numbers.js
@@ -1,6 +1,6 @@
 export const round = (value, decimalNumbers = 2) => {
   if (decimalNumbers > 0) {
-    return (Math.round(value * Math.pow(10, decimalNumbers)) / Math.pow(10, decimalNumbers));
+    return Math.round(value * Math.pow(10, decimalNumbers)) / Math.pow(10, decimalNumbers);
   } else {
     return Math.round(value);
   }
