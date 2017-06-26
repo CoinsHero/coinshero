03711a4a6b2db716e13b0eea8213b60c91521b9e
Or Kazaz <or.kazaz@autodesk.com>
2017-06-27 00:41:40 +0300

    Updated to webpack 3 after resolving the chunks issue + added the missing coins and placed them at the bottom of the list

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -1,1 +1,2 @@
 export const CLIENT_SIDE_DEBOUNCE_DELAY = 0;
+export const NO_VALUE_DATA_SYMBOL = '?';
--- src/redux/reducers/coinsReducer.js
+++ src/redux/reducers/coinsReducer.js
@@ -1,6 +1,7 @@
 import * as Immutable from 'seamless-immutable';
 import ValuePair from '../../models/ValuePair';
 import * as Actions from '../ActionNames';
+import {NO_VALUE_DATA_SYMBOL} from '../../helpers/consts';
 
 const initialState = Immutable.from({
   coinsList: {},
@@ -21,24 +22,64 @@ const CoinsReducer = (state = initialState, action) => {
     return state.merge({
       isUpdatingData: true
     });
-  case Actions.FETCH_COINS_DATA_SUCCESS:
+  case Actions.FETCH_COINS_DATA_SUCCESS: {
+    const coinsLength = action.payload.length;
+    const unSortedCoins = [];
+    const coins = [];
+    let index;
+    let rankIndex;
+
+    for (index = 0, rankIndex = 0; index < coinsLength; index++) {
+      const coin = action.payload[index];
+      const keys = Object.keys(coin);
+      const keysLength = keys.length;
+      let currentValue;
+
+      for (let fieldIndex = 0; fieldIndex < keysLength; fieldIndex++) {
+        switch (coin[keys[fieldIndex]]) {
+        case 'NaN':
+          currentValue = NO_VALUE_DATA_SYMBOL;
+          break;
+        case '':
+          currentValue = NO_VALUE_DATA_SYMBOL;
+          break;
+        default:
+          currentValue = coin[keys[fieldIndex]];
+          break;
+        }
+
+        coin[keys[fieldIndex]] = currentValue;
+      }
+
+      const coinBaseInfo = state.coinsList[coin.short.toLowerCase()];
+
+      if (coinBaseInfo) {
+        coin.imageUrl = coinBaseInfo.imageUrl;
+      }
+
+      const SORT_FIELD = 'mktcap';
+
+      // Unsorted
+      if (coin[SORT_FIELD] === NO_VALUE_DATA_SYMBOL) {
+        unSortedCoins.push(coin);
+      } else {
+        coins.push(ValuePair.parse(coin, action.meta.locale, rankIndex + 1));
+        rankIndex++;
+      }
+    }
+
+    for (index = 0, rankIndex = coins.length; index < unSortedCoins.length; index++, rankIndex++) {
+      coins.push(ValuePair.parse(unSortedCoins[index], action.meta.locale, rankIndex + 1));
+    }
+
     return state.merge({
       isUpdatingData: false,
       coinsData: {
-        valuePairs: action.payload
-          .filter((coin) => coin.mktcap && coin.mktcap !== 'NaN')
-          .map((coin, index) => {
-            const coinBaseInfo = state.coinsList[coin.short.toLowerCase()];
-
-            if (coinBaseInfo) {
-              coin.imageUrl = coinBaseInfo.imageUrl;
-            }
-
-            return ValuePair.parse(coin, action.meta.locale, index + 1);
-          }),
+        valuePairs: coins,
         updateTimestamp: Date.now()
       }
     });
+  }
   case Actions.FETCH_COINS_DATA_FAILURE:
     return state.merge({
       isUpdatingData: false
