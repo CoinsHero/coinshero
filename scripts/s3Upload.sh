057986e2e2bd9d0ef1e25a3cabe157e1232ff43e
Or Kazaz <or.kazaz@autodesk.com>
2017-07-22 19:29:20 +0300

    Change dthe deployment process to be much faster

--- scripts/s3Upload.sh
+++ scripts/s3Upload.sh
@@ -4,8 +4,9 @@ S3_BUCKET_URL=$1
 S3URL="${S3_BUCKET_URL}/$2"
 GRANTS=$3
 NUM_OF_DAYS_UNTIL_EXPIRATION=$4
-AWS_ACCESS_KEY_ID=$5
-AWS_SECRET_ACCESS_KEY=$6
+CLOUDFRONT_DISTRIBUTION_ID=$5
+AWS_ACCESS_KEY_ID=$6
+AWS_SECRET_ACCESS_KEY=$7
 
 Print() {
   echo ""
@@ -52,6 +53,17 @@ aws s3 ls ${S3URL}/latest --recursive | while read -r line;
           aws s3 rm "${fileToDelete}"
       fi
   done;
+
+Print "Invalidate CloudFront"
+
+# In our CloudFront distribution you should specify that the minimum TTL would be 31536000
+# That way we actually cache our index.html in CF and it doesn't have to go all the way to S3.
+# However, for our browser it will ALWAYS go to CF to get the file since it has Cache-Control: no-cache
+# So we're faster now, but in order to deploy new code we have to create an invalidation otherwise CF will keep the index.html file for
+# 1 year (=31536000).
+
+aws configure set preview.cloudfront true
+aws cloudfront create-invalidation --distribution-id "${CLOUDFRONT_DISTRIBUTION_ID}" --paths '/*'
 
 Print "Cleaning up the AWS credentials"
 
