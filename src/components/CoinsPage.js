009fb1291986c7d5f21db78d62a5395dd2990208
Omri Aharon <omri.aharon@gmail.com>
2017-07-23 13:06:15 +0300

    Omri/donations (#146)
    Donations

--- src/components/CoinsPage.js
+++ src/components/CoinsPage.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
 import { withStyles, createStyleSheet } from 'material-ui/styles';
 import { Toolbar, Typography } from 'material-ui';
 import T from 'i18n-react';
+import ClipboardButton from 'react-clipboard.js';
 
+import config from 'config';
 import classnamesjss from '../helpers/classnamesjss';
 
+import BitcoinLogo from '../assets/images/bitcoin_logo.png';
+import LitecoinLogo from '../assets/images/litecoin_logo.png';
+import EthereumLogo from '../assets/images/ethereum_logo.png';
+import SimpleSnackbar from './SimpleSnackbar';
 import CoinsTable from './CoinsTable';
 import SearchCoinsInput from './SearchCoinsInput';
 import TargetCurrencyMenu from './TargetCurrencyMenu';
@@ -38,6 +45,53 @@ const styleSheet = createStyleSheet('CoinsPage', (theme) => ({
   },
   'root__ToolBar__RightPanel__update-time-visible': {
     opacity: 1
+  },
+  root__ToolBar__DonateBar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    background: `linear-gradient(to bottom, ${theme.palette.accent.gray} 0%, ${theme.palette.accent.lightgray} 100%)`,
+    padding: '2px 4px',
+    borderRight: `1px solid ${theme.palette.accent['A400']}`,
+    borderBottom: `1px solid ${theme.palette.accent['A400']}`,
+    boxShadow: '1px 1px 4px rgba(0,0,0,0.075), inset 1px 1px 0px rgba(255,255,255,0.8)',
+    borderRadius: '2px 2px 2px 2px',
+    cursor: 'pointer',
+    '&:hover': {
+      background: `linear-gradient(to bottom, ${theme.palette.accent.lightgray} 0%, ${theme.palette.accent.gray} 100%)`
+    }
+  },
+  'root__ToolBar__DonateBar--dark-theme': {
+    background: `linear-gradient(to bottom, ${theme.palette.accent['A300']} 0%, #0a0a0a 100%)`,
+    '&:hover': {
+      background: `linear-gradient(to bottom, #0a0a0a 0%, ${theme.palette.accent['A300']} 100%)`
+    }
+  },
+  root__ToolBar__DonateBar__Icon: {
+    height: '15px',
+    width: '15px',
+    marginRight: '5px'
+  },
+  'root__ToolBar__DonateBar__Icon--rtl': {
+    marginLeft: theme.spacing.unit,
+    marginRight: 0
+  },
+  'root__ToolBar__snackbar--rtl': {
+    paddingLeft: '0',
+    '& div:nth-child(2)': {
+      paddingLeft: '0'
+    }
+  },
+  'root__ToolBar__hidden-copy-button': {
+    background: 'transparent',
+    border: 'none',
+    outline: 'none',
+    padding: 0,
+    marginLeft: theme.spacing.unit
+  },
+  'root__ToolBar__hidden-copy-button--rtl': {
+    marginLeft: 0,
+    marginRight: theme.spacing.unit
   }
 }));
 
@@ -47,7 +101,9 @@ class CoinsPage extends Component {
 
     this.state = {
       displayedValuePairs: props.coinsData.valuePairs,
-      searchQuery: ''
+      searchQuery: '',
+      snackbarOpen: false,
+      snackbarMessage: ''
     };
   }
 
@@ -103,21 +159,63 @@ class CoinsPage extends Component {
     );
   }
 
+  donate(snackbarMessage) {
+    this.setState({ snackbarMessage, snackbarOpen: true });
+  }
+
   render() {
+    const isRTL = this.props.locale.isRTL;
     const classes = this.props.classes;
+    const donateClass = classnamesjss(this.props.classes, 'root__ToolBar__DonateBar', {
+      'root__ToolBar__DonateBar--dark-theme': this.props.isDarkTheme
+    });
+    const logoClass = classnamesjss(this.props.classes, 'root__ToolBar__DonateBar__Icon', {
+      'root__ToolBar__DonateBar__Icon--rtl': isRTL
+    });
+    const snackbarClass = classnamesjss(this.props.classes, {
+      'root__ToolBar__snackbar--rtl': isRTL
+    });
+    const copyButton = classnamesjss(this.props.classes, 'root__ToolBar__hidden-copy-button', {
+      'root__ToolBar__hidden-copy-button--rtl': isRTL
+    });
 
     return (
       <div className={classes.root}>
         <Toolbar className={this.props.classes.root_ToolBar}>
           <div className={this.props.classes.root__ToolBar__LeftPanel}>
-            <SearchCoinsInput autoFocus={true} isRTL={this.props.locale.isRTL} onChange={this._onSearchChange.bind(this)}/>
+            <SearchCoinsInput autoFocus={true} isRTL={isRTL} onChange={this._onSearchChange.bind(this)}/>
             <TargetCurrencyMenu />
             <Typography type="caption">{ T.translate('USD_EURO_AVAILABLE') }</Typography>
+            <ClipboardButton className={ copyButton } data-clipboard-text={ config.DONATION.BITCOIN }
+              onClick={ () => this.donate(T.translate('DONATE_TEXT', { address: config.DONATION.BITCOIN, coin: 'Bitcoin' })) }>
+              <Typography type="caption" className={donateClass}><img src={ BitcoinLogo } className={logoClass} />
+                { T.translate('DONATE_BITCOIN') }
+              </Typography>
+            </ClipboardButton>
+            <ClipboardButton className={ copyButton } data-clipboard-text={ config.DONATION.ETHEREUM }
+              onClick={ () => this.donate(T.translate('DONATE_TEXT', { address: config.DONATION.ETHEREUM, coin: 'Ethereum' })) }>
+              <Typography type="caption" className={donateClass}><img src={ EthereumLogo } className={logoClass} />
+                <input id="ethereum_address" hidden readOnly value={ config.DONATION.ETHEREUM } />
+                { T.translate('DONATE_ETHEREUM') }
+              </Typography>
+            </ClipboardButton>
+            <ClipboardButton className={ copyButton } data-clipboard-text={ config.DONATION.LITECOIN }
+              onClick={ () => this.donate(T.translate('DONATE_TEXT', { address: config.DONATION.LITECOIN, coin: 'Litecoin' })) }>
+              <Typography type="caption" className={donateClass}>
+                <input id="litecoin_address" hidden readOnly value={ config.DONATION.LITECOIN } />
+                <img src={ LitecoinLogo } className={logoClass} />{ T.translate('DONATE_LITECOIN') }
+              </Typography>
+            </ClipboardButton>
           </div>
           <div className={this.props.classes.root__ToolBar__RightPanel}>
             {this._renderUpdateTime(this.props.coinsData.updateTimestamp)}
           </div>
         </Toolbar>
+        <SimpleSnackbar
+          className={ snackbarClass }
+          message={ this.state.snackbarMessage }
+          open={this.state.snackbarOpen}
+          onClose={ () => this.setState({ snackbarOpen: false })} />
         <CoinsTable locale={this.props.locale}
           showLoading={this.props.showLoading}
           valuePairs={this.state.displayedValuePairs} />
@@ -125,6 +223,10 @@ class CoinsPage extends Component {
     );
   }
 }
+
+const mapStateToProps = (state) => ({
+  isDarkTheme: state.site.isDarkTheme
+});
 
 CoinsPage.propTypes = {
   classes: PropTypes.object.isRequired,
@@ -136,7 +238,8 @@ CoinsPage.propTypes = {
     code: PropTypes.string,
     isRTL: PropTypes.bool
   }),
+  isDarkTheme: PropTypes.bool.isRequired,
   showLoading: PropTypes.bool.isRequired
 };
 
-export default withStyles(styleSheet)(CoinsPage);
+export default connect(mapStateToProps)(withStyles(styleSheet)(CoinsPage));
---
+++ src/components/SimpleSnackbar.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withStyles, createStyleSheet } from 'material-ui/styles';
+import Snackbar from 'material-ui/Snackbar';
+import IconButton from 'material-ui/IconButton';
+import CloseIcon from 'material-ui-icons/Close';
+
+const styleSheet = createStyleSheet('SimpleSnackbar', (theme) => ({
+  close: {
+    width: theme.spacing.unit * 4,
+    height: theme.spacing.unit * 4
+  }
+}));
+
+class SimpleSnackbar extends Component {
+  constructor(props) {
+    super(props);
+
+    this._handleRequestClose = this._handleRequestClose.bind(this);
+    this.state = {
+      message: null
+    };
+  }
+
+  _handleRequestClose(event, reason) {
+    this.props.onClose && this.props.onClose();
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    this.setState({ open: false });
+  }
+
+  render() {
+    const { className } = this.props;
+    return (
+      <div>
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right'
+          }}
+          open={this.props.open}
+          autoHideDuration={5000}
+          onRequestClose={this._handleRequestClose}
+          SnackbarContentProps={{
+            'aria-describedby': 'message-id',
+            className
+          }}
+          message={<span id="message-id">{ this.props.message }</span>}
+          action={[
+            <IconButton
+              key="close"
+              aria-label="Close"
+              color="inherit"
+              onClick={this._handleRequestClose}
+            >
+              <CloseIcon />
+            </IconButton>
+          ]}
+        />
+      </div>
+    );
+  }
+}
+
+SimpleSnackbar.propTypes = {
+  className: PropTypes.string,
+  open: PropTypes.bool.isRequired,
+  message: PropTypes.string.isRequired,
+  onClose: PropTypes.func
+};
+
+export default withStyles(styleSheet)(SimpleSnackbar);
