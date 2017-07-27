9a14bb2fad00737d26299b39e6fd252f1f561b46
Or Kazaz <or.kazaz89@gmail.com>
2017-07-27 12:41:13 +0300

    Moved the upload of new files to be on the bucket level itself (#147)

--- scripts/s3Upload.sh
+++ scripts/s3Upload.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 S3_BUCKET_URL=$1
-S3URL="${S3_BUCKET_URL}/$2"
-GRANTS=$3
-NUM_OF_DAYS_UNTIL_EXPIRATION=$4
-CLOUDFRONT_DISTRIBUTION_ID=$5
-AWS_ACCESS_KEY_ID=$6
-AWS_SECRET_ACCESS_KEY=$7
+S3URL="${S3_BUCKET_URL}"
+GRANTS=$2
+NUM_OF_DAYS_UNTIL_EXPIRATION=$3
+CLOUDFRONT_DISTRIBUTION_ID=$4
+AWS_ACCESS_KEY_ID=$5
+AWS_SECRET_ACCESS_KEY=$6
 
 Print() {
   echo ""
@@ -18,25 +18,20 @@ Print() {
 aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
 aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
 
-Print "Clear the previous folder and backup with current latest"
+Print "Sync files (only updated / additions) from the local dist folder to the S3 folder"
 
-aws s3 rm ${S3URL}/previous/ --recursive
-aws s3 cp ${S3URL}/latest ${S3URL}/previous --recursive --exclude "*" --include "*.html" ${GRANTS}
-
-Print "Sync files (only updated / additions) from the local dist folder to the S3 latest folder"
-
-aws s3 sync ./dist/ ${S3URL}/latest/ --exclude "*.html" --exclude "*.svg" ${GRANTS}  --cache-control "public, max-age=31536000"
-aws s3 sync ./dist/ ${S3URL}/latest/ --exclude "*" --include "*.svg" ${GRANTS} --cache-control "public, max-age=31536000" --content-type image/svg+xml
-aws s3 sync ./dist/ ${S3URL}/latest/ --exclude "*" --include "*.html" ${GRANTS} --cache-control no-cache
+aws s3 sync ./dist/ ${S3URL}/ --exclude "*.html" --exclude "*.svg" ${GRANTS}  --cache-control "public, max-age=31536000"
+aws s3 sync ./dist/ ${S3URL}/ --exclude "*" --include "*.svg" ${GRANTS} --cache-control "public, max-age=31536000" --content-type image/svg+xml
+aws s3 sync ./dist/ ${S3URL}/ --exclude "*" --include "*.html" ${GRANTS} --cache-control no-cache
 
 # on mac
 olderThan=`date -j -v-"${NUM_OF_DAYS_UNTIL_EXPIRATION}"d +%s`
 # on linux
 #olderThan=`date -d "-${NUM_OF_DAYS_UNTIL_EXPIRATION} days" +%s`
 
-Print "Remove files older than: ${olderThan} (${NUM_OF_DAYS_UNTIL_EXPIRATION} days) from the latest folder"
+Print "Remove files older than: ${olderThan} (${NUM_OF_DAYS_UNTIL_EXPIRATION} days)"
 
-aws s3 ls ${S3URL}/latest --recursive | while read -r line;
+aws s3 ls ${S3URL} --recursive | while read -r line;
   do
       createDate=`echo \$line | awk {'print \$1'}`
       filePath=`echo \$line | awk {'print \$4'}`
